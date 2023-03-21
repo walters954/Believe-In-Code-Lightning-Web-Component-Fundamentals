3047d9399c79a3306d21e1e94062db8a4f45a4c4
Warren Walters <warren.salesforce@gmail.com>
2023-03-20 21:21:20 -0500

    Added helloWorld LWC

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+    
+    greeting = 'World';
+    
+    changeHandler(event) {
+        this.greeting = event.target.value;
+    }
+}
